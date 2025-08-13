5317700a2e5487aeafe0e5604b8147c7c5fa5ddb
PyTom <107923033+PyyTom@users.noreply.github.com>
2025-08-13 13:49:22 +0000

    update

--- pages/EDITOR.py
+++ pages/EDITOR.py
@@ -1,23 +1,39 @@
 import sqlite3
 from flet import *
 def editor_view(page:Page,theme,alert):
-    def save_floors(e):
-        if t_floor.value=='' or t_rooms.value=='' or t_rooms.value==0:alert.title=Text('EMPTY FIELDS')
+    def add_to_config(e):
+        def remove(e):
+            e.control.parent.controls.remove(e.control)
+            page.update()
+        if t_floor.value=='' or t_rooms.value=='' or t_price.value=='':
+            alert.title=Text('EMPTY FIELDS')
+            page.open(alert)
         else:
-            db=sqlite3.connect('db.db')
-            if db.execute('select * from ROOMS where FLOOR=?',(t_floor.value,)).fetchall()==[]:
-                for r in range(1,int(t_rooms.value)+1):
-                    db.execute('insert into ROOMS values(?,?,?)',(t_floor.value,r,'green'))
-                    db.commit()
-                    alert.title=Text(f'FLOOR {t_floor.value} SUCCESSFULLY CREATED WITH {t_rooms.value} FREE ROOMS')
-                t_floor.value=0
-                t_rooms.value=0
-            else:alert.title=Text('FLOOR ALREADY EXISTS')
-            db.close()
+            c_config.controls.append(Container(content=Row([Text(str(t_floor.value),width=100),Text(str(t_rooms.value),width=100),Text(str(t_price.value),width=100)],alignment=MainAxisAlignment.CENTER),on_click=remove))
+            t_floor.value,t_rooms.value,t_price.value=0,0,0.0
+        page.update()
+    def save(e):    
+        db=sqlite3.connect('db.db')
+        for c in c_config.controls:
+            print(c)
+            for t in c.content.controls:print(t)
+            data=[t.value for t in c.content.controls]
+            data.insert(2,'green')
+            db.execute('insert into ROOMS values(?,?,?,?)',data)
+            db.commit()
+            alert.title=Text(f'FLOOR {data[0]} SUCCESSFULLY: {data[1]} FREE ROOMS FOR $.{data[2]} PER NIGHT')
+        db.close()
         page.open(alert)
         page.update()
-    t_floor=TextField(label='FLOOR',keyboard_type=KeyboardType.NUMBER,value=0)
-    t_rooms=TextField(label='TOTAL ROOMS',keyboard_type=KeyboardType.NUMBER,value=0)
+    t_floor=TextField(label='FLOOR',keyboard_type=KeyboardType.NUMBER,value=0,width=100)
+    t_rooms=TextField(label='ROOMS',keyboard_type=KeyboardType.NUMBER,value=0,width=100)
+    t_price=TextField(label='PRICE',keyboard_type=KeyboardType.NUMBER,value=0.0,width=100)
+    c_config=Column()
     return Column([Row([Switch(on_change=theme),Text('daHotel',color='orange',size=30),IconButton(icon=Icons.EXIT_TO_APP,icon_color='red',icon_size=50,on_click=lambda _:page.go('/HOME'))],alignment=MainAxisAlignment.CENTER),
                    Divider(),
-                   Row([Column([t_floor,t_rooms,ElevatedButton('SAVE',on_click=save_floors)])],alignment=MainAxisAlignment.CENTER)])
+                   Row([t_floor,t_rooms,t_price,ElevatedButton('ADD TO CONFIGURATION',on_click=add_to_config)],alignment=MainAxisAlignment.CENTER),
+                   Divider(),
+                   Row([Text('ROOMS CONFIGURATION',size=20,color='orange')],alignment=MainAxisAlignment.CENTER),
+                   Row([Text('FLOOR',width=100),Text('ROOMS',width=100),Text('PRICE',width=100)],alignment=MainAxisAlignment.CENTER),
+                   Row([c_config],alignment=MainAxisAlignment.CENTER),
+                   Row([ElevatedButton('SAVE',on_click=save)],alignment=MainAxisAlignment.CENTER)])
